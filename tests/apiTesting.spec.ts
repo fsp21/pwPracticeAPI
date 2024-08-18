c2e5fb741c9e10942692ab1f7f8c4653c18d48b6
unknown <felipeasaopaulo@gmail.com>
2024-08-18 20:55:41 -0300

    intercepting requests and altering values

--- tests/apiTesting.spec.ts
+++ tests/apiTesting.spec.ts
@@ -1,34 +1,38 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach('Navigate to URL', async ({page}) =>{
-  await page.route('**/*tags', async route =>{
-    const tags = {
-      "tags": [
-          "asdf",
-          "GitHasdfub",
-          "Coding",
-          "Bondaasdfr Academy",
-          "Gdddit",
-          "Zoom",
-          "YouTube",
-          "qa career",
-          "Collaboration",
-          "Value-Focused"
-      ]
-  }
-  await route.fulfill({
-    status: 200,
-    contentType: 'application/json; charset=utf-8',
-    body: JSON.stringify(tags)
-  })
+test('Navigate to URL', async ({page}) =>{
   
+  await page.route('**/*api/articles*', async route => {
+    const response = await route.fetch();
+    const responseBody = await response.json()
+    responseBody.articles[0].title = 'Test Title'
+    responseBody.articles[0].description = 'Test Description'
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
   })
+
   await page.goto('https://conduit.bondaracademy.com/');
+  await page.waitForTimeout(2000)
+  await expect(page.locator('app-article-list app-article-preview').first().locator('h1')).toContainText('Test Title');
+  await expect(page.locator('app-article-list app-article-preview').first().locator('p')).toContainText('Test Description');
 })
 
 test('Check if text is displayed', async ({ page }) => {
+  await page.route('**/*tags', async route =>{
+      const tags = {
+        "tags": [
+            "asdf","GitHasdfub", "Bondaasdfr"
+        ]
+      }
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json; charset=utf-8',
+      body: JSON.stringify(tags)
+    }) 
+  })
+  await page.goto('https://conduit.bondaracademy.com/');
   await page.waitForTimeout(2000)
-  //await expect(page.locator('.navbar-brand')).toHaveText('conduit');
   await expect(page.locator('div', {hasText: "Popular Tags"})).toContainText(["asdf","GitHasdfub", "Bondaasdfr"])
 });
 
