be01cb836e2678e352e473de94296bb105e2364c
unknown <felipeasaopaulo@gmail.com>
2024-08-19 07:33:34 -0300

    Practicing making requests

--- tests/apiTesting.spec.ts
+++ tests/apiTesting.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Navigate to URL', async ({page}) =>{
+test('Navigate to URL [mocking API response entirely]', async ({page}) =>{
   
   await page.route('**/*api/articles*', async route => {
     const response = await route.fetch();
@@ -13,12 +13,12 @@ test('Navigate to URL', async ({page}) =>{
   })
 
   await page.goto('https://conduit.bondaracademy.com/');
-  await page.waitForTimeout(2000)
+  await page.waitForTimeout(2000) // small timeout for Playwright to wait until the modified request is processed
   await expect(page.locator('app-article-list app-article-preview').first().locator('h1')).toContainText('Test Title');
   await expect(page.locator('app-article-list app-article-preview').first().locator('p')).toContainText('Test Description');
 })
 
-test('Check if text is displayed', async ({ page }) => {
+test('Check if text is displayed [modifying response from API]', async ({ page }) => {
   await page.route('**/*tags', async route =>{
       const tags = {
         "tags": [
@@ -32,7 +32,25 @@ test('Check if text is displayed', async ({ page }) => {
     }) 
   })
   await page.goto('https://conduit.bondaracademy.com/');
-  await page.waitForTimeout(2000)
+  await page.waitForTimeout(2000) // small timeout for Playwright to wait until the modified request is processed
   await expect(page.locator('div', {hasText: "Popular Tags"})).toContainText(["asdf","GitHasdfub", "Bondaasdfr"])
 });
 
+test('Making requests', async ({ page, request }) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {"user":{"email":"testEmail@testmail.net","password":"Welcome1"}}
+  })
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/',{
+    data: {
+      "article":{"title":"m8ock article title test1234","description":"9mock article description test1234","body":"1234mock article body test","tagList":[]}
+    },
+    headers: {
+      "Authorization": `Token ${accessToken}`
+    }
+  })
+  expect(articleResponse.status()).toBe(201)
+
+})
